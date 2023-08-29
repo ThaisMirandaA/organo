fc3b98be3386351f97c13008aecb6ba3eeb1383e
Thais <thaismirandadealmeida@gmail.com>
2023-08-29 14:47:05 -0300

    Adding button to show and hide form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,13 +4,13 @@ import ListaSuspensa from '../ListaSuspensa'
 import Botao from '../Botao'
 import { useState } from 'react'
 
-const Formulario = ({times, aoNovoColaboradorCadastrado, aoNovoTimeCadastrado}) => {
-    
+const Formulario = ({ times, aoNovoColaboradorCadastrado, aoNovoTimeCadastrado }) => {
+
     const aoSalvar = (evento) => {
         evento.preventDefault();
         aoNovoColaboradorCadastrado({
-            nome, 
-            cargo, 
+            nome,
+            cargo,
             imagem,
             time
         })
@@ -37,64 +37,69 @@ const Formulario = ({times, aoNovoColaboradorCadastrado, aoNovoTimeCadastrado}) 
     const [nomeTime, setNomeTime] = useState('')
     const [corTime, setCorTime] = useState('')
 
+    const [mostraFormulario, setMostraFormulario] = useState(false)
+    const mostraEOculta = () => setMostraFormulario(!mostraFormulario)
 
     return (
-        <section className='formulario'>
-            <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do colaborador:</h2>
-                <Campo
-                    obrigatorio={true}
-                    label="Nome"
-                    placeholder="Digite seu nome"
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}
-                />
-                <Campo
-                    obrigatorio={true}
-                    label="Cargo"
-                    placeholder="Digite seu cargo"
-                    valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}
-                />
-                <Campo
-                    label="Imagem"
-                    placeholder="Informe o endereço da imagem"
-                    valor={imagem}
-                    aoAlterado={valor => setImagem(valor)}
-                />
-                <ListaSuspensa
-                    obrigatorio={true}
-                    label="Time"
-                    itens={times}
-                    valor={time}
-                    aoAlterado={valor =>setTime(valor)}
-                />
-                <Botao>
-                    Criar Card
-                </Botao>
-            </form>
-            <form onSubmit={aoCriarNovoTime}>
-                <h2>Preencha os dados para criar um novo time:</h2>
-                <Campo
-                    obrigatorio={true}
-                    label="Nome do time"
-                    placeholder="Digite o nome do time"
-                    valor={nomeTime}
-                    aoAlterado={valor => setNomeTime(valor)}
-                />
-                <Campo
-                    obrigatorio={true}
-                    type='color'
-                    label="Cor do time"
-                    placeholder="Digite a cor do time"
-                    valor={corTime}
-                    aoAlterado={valor => setCorTime(valor)}
-                />
-                <Botao>
-                    Criar Novo Time
-                </Botao>
-            </form>
-        </section>
+        <div>
+            <button className='botao-add'onClick={mostraEOculta}><img src='/imagens/botaoadd.png' alt='Botão que adiciona formulário na tela'/></button>
+            {mostraFormulario ? <section className='formulario'>
+                <form onSubmit={aoSalvar}>
+                    <h2>Preencha os dados para criar o card do colaborador:</h2>
+                    <Campo
+                        obrigatorio={true}
+                        label="Nome"
+                        placeholder="Digite seu nome"
+                        valor={nome}
+                        aoAlterado={valor => setNome(valor)}
+                    />
+                    <Campo
+                        obrigatorio={true}
+                        label="Cargo"
+                        placeholder="Digite seu cargo"
+                        valor={cargo}
+                        aoAlterado={valor => setCargo(valor)}
+                    />
+                    <Campo
+                        label="Imagem"
+                        placeholder="Informe o endereço da imagem"
+                        valor={imagem}
+                        aoAlterado={valor => setImagem(valor)}
+                    />
+                    <ListaSuspensa
+                        obrigatorio={true}
+                        label="Time"
+                        itens={times}
+                        valor={time}
+                        aoAlterado={valor => setTime(valor)}
+                    />
+                    <Botao>
+                        Criar Card
+                    </Botao>
+                </form>
+                <form onSubmit={aoCriarNovoTime}>
+                    <h2>Preencha os dados para criar um novo time:</h2>
+                    <Campo
+                        obrigatorio={true}
+                        label="Nome do time"
+                        placeholder="Digite o nome do time"
+                        valor={nomeTime}
+                        aoAlterado={valor => setNomeTime(valor)}
+                    />
+                    <Campo
+                        obrigatorio={true}
+                        type='color'
+                        label="Cor do time"
+                        placeholder="Digite a cor do time"
+                        valor={corTime}
+                        aoAlterado={valor => setCorTime(valor)}
+                    />
+                    <Botao>
+                        Criar Novo Time
+                    </Botao>
+                </form>
+            </section> : null }
+        </div>
     )
 }
 
